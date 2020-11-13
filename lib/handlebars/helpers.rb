ebf42c81d30751275229e982c37bb8f291502373
klueless-io <david@ideasmen.com.au>
2020-11-13 20:38:37 +1100

    first commit

---
+++ lib/handlebars/helpers.rb
@@ -0,0 +1,8 @@
+require "handlebars/helpers/version"
+
+module Handlebars
+  module Helpers
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/handlebars/helpers/version.rb
@@ -0,0 +1,5 @@
+module Handlebars
+  module Helpers
+    VERSION = "0.1.0"
+  end
+end
