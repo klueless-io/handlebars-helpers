0f7ac9c030b46306aded128f872b0872c125f754
klueless-io <david@ideasmen.com.au>
2021-01-17 11:12:06 +1100

    upgrade tokenizer to support prefix/suffix numbers

--- spec/handlebars/string_tokenizer_spec.rb
+++ spec/handlebars/string_tokenizer_spec.rb
@@ -3,7 +3,16 @@
 require 'handlebars/helpers/string_tokenizer'
 
 RSpec.describe Handlebars::Helpers::StringTokenizer do
-  let(:subject) { described_class.parse(value) }
+  let(:subject) do
+    described_class.parse(value,
+                          preserve_case: preserve_case,
+                          compress_prefix_numerals: compress_prefix_numerals,
+                          compress_suffix_numerals: compress_suffix_numerals)
+  end
+
+  let(:preserve_case) { false }
+  let(:compress_prefix_numerals) { true }
+  let(:compress_suffix_numerals) { true }
 
   context 'with character handling' do
     context 'when nil' do
@@ -40,6 +49,44 @@ RSpec.describe Handlebars::Helpers::StringTokenizer do
       let(:value) { 'THE QUICK BROWN FOX' }
 
       it { is_expected.to eq('the-quick-brown-fox') }
+    end
+
+    context 'when given a camel case' do
+      let(:value) { 'TheQuickBrownFox' }
+
+      it { is_expected.to eq('the-quick-brown-fox') }
+    end
+
+    context 'when given a lamel case' do
+      let(:value) { 'theQuickBrownFox' }
+
+      it { is_expected.to eq('the-quick-brown-fox') }
+    end
+
+    context 'handle numbers' do
+      context 'when suffixed with number' do
+        let(:value) { 'the quick brown fox 66' }
+
+        it { is_expected.to eq('the-quick-brown-fox66') }
+
+        context 'when compress_suffix_numerals is false' do
+          let(:compress_suffix_numerals) { false }
+
+          it { is_expected.to eq('the-quick-brown-fox-66') }
+        end
+      end
+
+      context 'when prefixed with number' do
+        let(:value) { '99 the quick brown fox' }
+
+        it { is_expected.to eq('99the-quick-brown-fox') }
+
+        context 'when compress_suffix_numerals is false' do
+          let(:compress_prefix_numerals) { false }
+
+          fit { is_expected.to eq('99-the-quick-brown-fox') }
+        end
+      end
     end
   end
 end
