410d6c228fe587f4071c4a1efa05128ef6e27d66
klueless-io <david@ideasmen.com.au>
2021-01-21 22:40:36 +1100

    add string.padr, padl and configuration options for padding

---
+++ lib/handlebars/helpers/string_formatting/padr.rb
@@ -0,0 +1,59 @@
+# frozen_string_literal: true
+
+# reference: https://github.com/rails/rails/blob/master/activesupport/lib/active_support/inflector/methods.rb
+require 'active_support/core_ext/string'
+
+require 'handlebars/helpers/base_helper'
+require 'handlebars/helpers/configuration'
+
+module Handlebars
+  module Helpers
+    # String manipulation methods for case formatting
+    module StringFormatting
+      # Add padding to the right of the value.
+      class Padr < Handlebars::Helpers::BaseHelper
+        # Parse will Add padding to the right of the value.
+        #
+        # @example
+        #
+        #   puts "[#{Padr.new.parse('aaa', 10)}]"
+        #
+        #   [aaa       ]
+        #
+        # @example
+        #
+        #   puts "[#{Padr.new.parse('aaa')}]"
+        #
+        #   [aaa                           ]
+        #
+        # @example
+        #
+        #   puts Padr.new.parse('aaa', '10', '-')
+        #
+        #   aaa-------
+        #
+        # @param [String] value - value to apply padding to
+        # @param [Integer] count - how much padding to apply. defaults to configuration.padr_count
+        # @param [String] char - character to pad with. defaults to configuration.padr_char
+        # @return [String] value with padding to right
+        def parse(value, count = nil, char = nil)
+          value = '' if value.nil?
+          count = Handlebars::Helpers.configuration.padr_count if count.nil?
+          count = count.to_i if count.is_a?(String)
+          char = Handlebars::Helpers.configuration.padr_char if char.nil?
+          value.ljust(count, char)
+        end
+
+        def handlebars_helper
+          proc do |_context, value, count, char|
+            # Handle optional: value, count and char
+            value = nil if value.is_a?(V8::Object)
+            count = nil if count.is_a?(V8::Object)
+            char = nil if char.is_a?(V8::Object)
+            parse(value, count, char)
+          end
+        end
+      end
+    end
+  end
+end
